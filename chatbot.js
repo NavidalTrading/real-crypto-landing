9e15c4f458e9c3942a773d9fe4d3894cc23e7d86
NavidalTrading <111570404+NavidalTrading@users.noreply.github.com>
2025-07-26 18:17:34 +0300

    Add files via upload

---
+++ chatbot.js
@@ -0,0 +1,7 @@
+
+// Crypto Daniel Chatbot Logic
+window.addEventListener("message", function(event) {
+  if (event.data === "open-chat") {
+    alert("Crypto Daniel is here to assist you!");
+  }
+});
